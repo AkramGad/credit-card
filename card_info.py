05a0d96f6ae299c9c28d1c3743bd4c6906b03702
AkramGad <115922751+AkramGad@users.noreply.github.com>
2022-12-01 09:58:50 +0200

    Add files via upload
    starting saving credit card code

---
+++ card_info.py
@@ -0,0 +1,15 @@
+dict_save = {
+    1: "Yes",
+    2: "No"
+}
+
+def saveinfo():
+    print("Would you like to save your credit card information? \n", dict_save)
+    key = int(input("Please choose an answer: "))
+    if key == 1:
+        pass
+    elif key == 2:
+        pass
+
+def creditcard():
+    
